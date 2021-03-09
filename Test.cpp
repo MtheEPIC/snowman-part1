f2a3bea14edb7f83a68516937e2cefb76ce130c2
MtheEPIC <mtheepic1423@protonmail.com>
2021-03-09 19:33:48 +0200

    changed tests to positive

--- Test.cpp
+++ Test.cpp
@@ -1,6 +1,6 @@
 #include "doctest.h"
 #include "snowman.hpp"
-#include <string>
+#include <string.h>
 
 using namespace ariel;
 using namespace std;
@@ -15,19 +15,19 @@ TEST_CASE("Good snowman example")
     CHECK(snowman(11114411) == string("       \n _===_ \n (.,.) \n ( : ) \n ( : ) "));
 }
 
-TEST_CASE("Bad snowman input length")
+TEST_CASE("Good snowman input length")
 {
     CHECK_THROWS(snowman(111144114));
     CHECK_THROWS(snowman(1111441));
-    CHECK_THROWS(snowman(NULL));
+    //CHECK_THROWS(snowman(NULL));
 }
 
-TEST_CASE("Bad snowman output length")
+TEST_CASE("Good snowman output length")
 {
-    CHECK(snowman(11114411).length() != 39);
+    CHECK(snowman(11114411).length() == 39);
 }
 
-TEST_CASE("Bad snowman values")
+TEST_CASE("Good snowman values")
 {
     CHECK_THROWS(snowman(11104411));
     CHECK_THROWS(snowman(11154411));
@@ -36,66 +36,66 @@ TEST_CASE("Bad snowman values")
     CHECK_THROWS(snowman(-11114411));
 }
 
-TEST_CASE("Bad snowman hat")
+TEST_CASE("Good snowman hat")
 {
-    CHECK(snowman(11114411).substr(0, 15) != string("       \n _===_ \n"));
-    CHECK(snowman(21114411).substr(0, 15) != string("  ___  \n ..... \n"));
-    CHECK(snowman(31114411).substr(0, 15) != string("   _   \n  /_\\  \n"));
-    CHECK(snowman(41114411).substr(0, 15) != string("  ___  \n (_*_) \n"));
+    CHECK(snowman(11114411).substr(0, 16) == string("       \n _===_ \n"));
+    CHECK(snowman(21114411).substr(0, 16) == string("  ___  \n ..... \n"));
+    CHECK(snowman(31114411).substr(0, 16) == string("   _   \n  /_\\  \n"));
+    CHECK(snowman(41114411).substr(0, 16) == string("  ___  \n (_*_) \n"));
 }
 
-TEST_CASE("Bad snowman nose")
+TEST_CASE("Good snowman nose")
 {
-    CHECK(snowman(11114411)[19] != ',');
-    CHECK(snowman(12114411)[19] != '.');
-    CHECK(snowman(13114411)[19] != '_');
-    CHECK(snowman(14114411)[19] != ' ');
+    CHECK(snowman(11114411)[19] == ',');
+    CHECK(snowman(12114411)[19] == '.');
+    CHECK(snowman(13114411)[19] == '_');
+    CHECK(snowman(14114411)[19] == ' ');
 }
 
-TEST_CASE("Bad snowman left eye")
+TEST_CASE("Good snowman left eye")
 {
-    CHECK(snowman(11114411)[18] != '.');
-    CHECK(snowman(11214411)[18] != 'o');
-    CHECK(snowman(11314411)[18] != 'O');
-    CHECK(snowman(11414411)[18] != '-');
+    CHECK(snowman(11114411)[18] == '.');
+    CHECK(snowman(11214411)[18] == 'o');
+    CHECK(snowman(11314411)[18] == 'O');
+    CHECK(snowman(11414411)[18] == '-');
 }
 
-TEST_CASE("Bad snowman right eye")
+TEST_CASE("Good snowman right eye")
 {
-    CHECK(snowman(11114411)[20] != '.');
-    CHECK(snowman(11124411)[20] != 'o');
-    CHECK(snowman(11134411)[20] != 'O');
-    CHECK(snowman(11144411)[20] != '-');
+    CHECK(snowman(11114411)[20] == '.');
+    CHECK(snowman(11124411)[20] == 'o');
+    CHECK(snowman(11134411)[20] == 'O');
+    CHECK(snowman(11144411)[20] == '-');
 }
 
-TEST_CASE("Bad snowman left arm")
+TEST_CASE("Good snowman left arm")
 {
-    CHECK((snowman(11111411)[16] != ' ' || snowman(11111411)[24] != '<'));
-    CHECK((snowman(11112411)[16] != '\\' || snowman(11112411)[24] != ' '));
-    CHECK((snowman(11113411)[16] != ' ' || snowman(11113411)[24] != '/'));
-    CHECK((snowman(11114411)[16] != ' ' || snowman(11114411)[24] != ' '));
+    CHECK((snowman(11111411)[16] == ' ' && snowman(11111411)[24] == '<'));
+    CHECK((snowman(11112411)[16] == '\\' && snowman(11112411)[24] == ' '));
+    CHECK((snowman(11113411)[16] == ' ' && snowman(11113411)[24] == '/'));
+    CHECK((snowman(11114411)[16] == ' ' && snowman(11114411)[24] == ' '));
 }
 
-TEST_CASE("Bad snowman right arm")
+TEST_CASE("Good snowman right arm")
 {
-    CHECK((snowman(11114111)[22] != ' ' || snowman(11114111)[30] != '>'));
-    CHECK((snowman(11114211)[22] != '/' || snowman(11114211)[30] != ' '));
-    CHECK((snowman(11114311)[22] != ' ' || snowman(11114311)[30] != '\\'));
-    CHECK((snowman(11114411)[22] != ' ' || snowman(11114411)[30] != ' '));
+    CHECK((snowman(11114111)[22] == ' ' && snowman(11114111)[30] == '>'));
+    CHECK((snowman(11114211)[22] == '/' && snowman(11114211)[30] == ' '));
+    CHECK((snowman(11114311)[22] == ' ' && snowman(11114311)[30] == '\\'));
+    CHECK((snowman(11114411)[22] == ' ' && snowman(11114411)[30] == ' '));
 }
 
-TEST_CASE("Bad snowman torso")
+TEST_CASE("Good snowman torso")
 {
-    CHECK(snowman(11114411).substr(26, 29) != string(" : "));
-    CHECK(snowman(11114421).substr(26, 29) != string("] ["));
-    CHECK(snowman(11114431).substr(26, 29) != string("> <"));
-    CHECK(snowman(11114441).substr(26, 29) != string("   "));
+    CHECK(snowman(11114411).substr(26, 3) == string(" : "));
+    CHECK(snowman(11114421).substr(26, 3) == string("] ["));
+    CHECK(snowman(11114431).substr(26, 3) == string("> <"));
+    CHECK(snowman(11114441).substr(26, 3) == string("   "));
 }
 
-TEST_CASE("Bad snowman base")
+TEST_CASE("Good snowman base")
 {
-    CHECK(snowman(11114411).substr(34, 37) != string(" : "));
-    CHECK(snowman(11114411).substr(34, 37) != string("\" \""));
-    CHECK(snowman(11114411).substr(34, 37) != string("___"));
-    CHECK(snowman(11114411).substr(34, 37) != string("   "));
+    CHECK(snowman(11114411).substr(34, 3) == string(" : "));
+    CHECK(snowman(11114412).substr(34, 3) == string("\" \""));
+    CHECK(snowman(11114413).substr(34, 3) == string("___"));
+    CHECK(snowman(11114414).substr(34, 3) == string("   "));
 }
